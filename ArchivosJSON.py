85b4ef2ab81f1b3ec574da0878a97ec16af94688
LEGS2001 <69339576+LEGS2001@users.noreply.github.com>
2021-05-16 17:51:41 -0500

    Update ArchivosJSON.py
    Optimización del código

--- ArchivosJSON.py
+++ ArchivosJSON.py
@@ -3,28 +3,29 @@ import random
 
 
 
-idVehiculo = 301
+idVehiculo = 1
 lista_tipos = ["BUS ESCOLAR","AUTO PARTICULAR","MOTOCICLETAS","CAMION","BUS PUBLICO","TAXI"]
 lista_sectores = ["SAMBORONDON", "DAULE", "LA ALBORADA", "BELLAVISTA"]
-lista_horas = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
+
+dic_list = []
 
 for i in range(0, 100):
 
+    horas = random.randint(0, 23)
     minutos = random.randint(0,59)
-    if minutos <= 9:
-        minutos = "0" + str(minutos)
 
     dic = {
         "IdCarro" : idVehiculo,
-        "tipo" : lista_tipos[random.randint(0,len(lista_tipos)-1)],
-        "hora" : str(lista_horas[random.randint(0,len(lista_horas)-1)] +  str(minutos)),
-        "sector" : lista_sectores[random.randint(0,len(lista_sectores)-1)],
+        "tipo" : random.choice(lista_tipos),
+        "hora" : str(horas).zfill(2) + ":" +str(minutos).zfill(2),
+        "sector" : random.choice(lista_sectores),
         "velocidad" : random.randint(60,121)
     }
 
-    json_object = json.dumps(dic, indent = 4)
-
-    with open("Vehiculos.json", "a") as outfile:
-        outfile.write(json_object + "," + "\n")
-
+    dic_list.append(dic)
     idVehiculo += 1
+
+json_object = json.dumps(dic_list, indent = 0)
+
+with open("Vehiculos.json", "a") as outfile:
+    outfile.write(json_object)
